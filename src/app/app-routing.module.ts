42b1f7763a21fb1919b3f95fd5d9fe36191ddeee
farbfetzen <bastihz.dev@posteo.de>
2022-08-05 20:16:18 +0200

    Make A the default component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { BComponent } from './b/b.component';
 import { CComponent } from './c/c.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/c' },
+  { path: '', pathMatch: 'full', redirectTo: '/a' },
   { path: 'a', component: AComponent },
   { path: 'b', component: BComponent },
   { path: 'c', component: CComponent },
